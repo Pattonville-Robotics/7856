f8d9e56d9cb72bcf47a2ce72934b3ef0e7f58fd8
greg-bahr <greg.bahr123@gmail.com>
2016-09-29 19:05:14 -0500

    Test TeleOp for CandyBot

---
+++ src/main/java/pattonvillerobotics/opmodes/TeleOp/TestTeleOp.java
@@ -0,0 +1,37 @@
+package pattonvillerobotics.opmodes.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.pattonvillerobotics.commoncode.robotclasses.drive.AbstractDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.SimpleDrive;
+
+/**
+ * Created by bahrg on 9/27/16.
+ */
+
+@TeleOp(name="candyBot", group="Test")
+public class TestTeleOp extends LinearOpMode {
+
+    private AbstractDrive drive;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+        waitForStart();
+
+        while(opModeIsActive()) {
+            doLoop();
+            idle();
+        }
+    }
+
+    public void initialize() {
+        drive = new SimpleDrive(this, hardwareMap);
+        telemetry.addData("Init", "Initialized.");
+    }
+
+    public void doLoop() {
+        drive.moveFreely(gamepad1.left_stick_y, -gamepad1.right_stick_y);
+    }
+}
