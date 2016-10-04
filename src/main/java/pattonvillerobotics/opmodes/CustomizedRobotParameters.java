78384674aeadca788ed55823850622c45a7eae8f
greg-bahr <greg.bahr123@gmail.com>
2016-10-04 15:06:45 -0500

    Customized Robot Params

---
+++ src/main/java/pattonvillerobotics/opmodes/CustomizedRobotParameters.java
@@ -0,0 +1,31 @@
+package pattonvillerobotics.opmodes;
+
+import org.pattonvillerobotics.commoncode.robotclasses.drive.RobotParameters;
+
+/**
+ * Created by bahrg on 10/4/16.
+ */
+
+public class CustomizedRobotParameters {
+    public static final RobotParameters ROBOT_PARAMETERS;
+    public static final RobotParameters TESTROBOT_PARAMETERS;
+
+
+    static {
+        ROBOT_PARAMETERS = new RobotParameters.Builder()
+                    .encodersEnabled(true)
+                    .gyroEnabled(false)
+                    .wheelRadius(2)
+                    .wheelBaseRadius(7.5)
+                    .driveGearRatio(2)
+                    .build();
+
+        TESTROBOT_PARAMETERS = new RobotParameters.Builder()
+                .encodersEnabled(true)
+                .gyroEnabled(false)
+                .driveGearRatio(6)
+                .wheelBaseRadius(6.25)
+                .wheelRadius(2)
+                .build();
+    }
+}
