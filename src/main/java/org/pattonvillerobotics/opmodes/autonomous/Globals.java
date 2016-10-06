81c5bb88114071139cddc6adb12ea8bba263fb88
Michael Pieper <michaelepieper@gmail.com>
2016-10-06 18:10:01 -0500

    Globals Class
    Created Globals class for parameters and other global constants.

---
+++ src/main/java/org/pattonvillerobotics/opmodes/autonomous/Globals.java
@@ -0,0 +1,12 @@
+package org.pattonvillerobotics.opmodes.autonomous;
+
+/**
+ * Created by pieperm on 10/6/16.
+ */
+
+public class Globals {
+
+    public static final double MAX_MOTOR_POWER = 0.7;
+    public static final double HALF_MOTOR_POWER = 0.5;
+    
+}
