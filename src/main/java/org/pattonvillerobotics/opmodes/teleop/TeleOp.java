f5a25ecc30858390f246513692d044204d7e0f8f
skaggsw <williampegs@gmail.com>
2016-10-11 15:16:56 -0500

    Work on finishing the TeleOp to move the arm when the A button is presssed.

--- src/main/java/org/pattonvillerobotics/opmodes/teleop/TeleOp.java
+++ src/main/java/org/pattonvillerobotics/opmodes/teleop/TeleOp.java
@@ -1,8 +1,12 @@
 package org.pattonvillerobotics.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.GamepadData;
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableButton;
+import org.pattonvillerobotics.commoncode.robotclasses.gamepad.ListenableGamepad;
 import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
 
 /**
@@ -12,6 +16,8 @@ import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
 public class TeleOp extends LinearOpMode {
 
     private EncoderDrive drive;
+    private ListenableGamepad gamepad;
+    private Servo servo;
 
     public void runOpMode() throws InterruptedException {
         initialize();
@@ -26,9 +32,25 @@ public class TeleOp extends LinearOpMode {
     public void initialize() {
         drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
         telemetry.addData("Init", "Initialized.");
+        this.servo = hardwareMap.servo.get("arm");
+        gamepad.getButton(GamepadData.Button.A).addListener(ListenableButton.ButtonState.BEING_PRESSED, new ListenableButton.ButtonListener() {
+            @Override
+            public void run() {
+                servo.
+
+            }
+        });
+        gamepad.getButton(GamepadData.Button.A).addListener(ListenableButton.ButtonState.JUST_PRESSED, new ListenableButton.ButtonListener() {
+            @Override
+            public void run() {
+
+
+            }
+        });
     }
 
     public void doLoop() {
         drive.moveFreely(gamepad1.left_stick_y, -gamepad1.right_stick_y);
+
     }
 }
