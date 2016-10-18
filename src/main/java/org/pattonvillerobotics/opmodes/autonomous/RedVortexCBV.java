7bce84c6a4a68005e24ee1417358f7d74a3cdd2f
Michael Pieper <michaelepieper@gmail.com>
2016-10-18 15:56:29 -0500

    Fixed NullPointerException, created init method that takes in our EncoderDrive and AllianceColor, called before waitForStart()

---
+++ src/main/java/org/pattonvillerobotics/opmodes/autonomous/RedVortexCBV.java
@@ -0,0 +1,28 @@
+package org.pattonvillerobotics.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
+
+/**
+ * Created by pieperm on 10/18/16.
+ */
+
+public class RedVortexCBV extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+        waitForStart();
+
+        AutoMethods.runProcessCBV();
+
+    }
+
+    public void initialize() {
+        AutoMethods.init(new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS), AllianceColor.RED, StartPosition.VORTEX);
+    }
+}
---
+++ src/main/java/org/pattonvillerobotics/opmodes/autonomous/StartPosition.java
@@ -0,0 +1,9 @@
+package org.pattonvillerobotics.opmodes.autonomous;
+
+/**
+ * Created by pieperm on 10/18/16.
+ */
+
+public enum StartPosition {
+    VORTEX, LINE
+}
