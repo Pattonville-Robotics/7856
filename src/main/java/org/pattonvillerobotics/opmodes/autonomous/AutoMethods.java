4240ccd4873ca5e8d90fd368d3ebc84af2f980cd
Michael Pieper <michaelepieper@gmail.com>
2016-10-27 19:58:54 -0500

    Reworked alignToBeacon method to use motor powers instead of degree turns

--- src/main/java/org/pattonvillerobotics/opmodes/autonomous/AutoMethods.java
+++ src/main/java/org/pattonvillerobotics/opmodes/autonomous/AutoMethods.java
@@ -126,10 +126,10 @@ public class AutoMethods {
                     double offset = VuforiaNav.getxPos(lastLocation);
 
                     if(offset > Globals.BEACON_MAXIMUM_OFFSET) {
-                        drive.rotateDegrees(Direction.LEFT, Globals.BEACON_ALIGN_TURN, Globals.HALF_MOTOR_POWER);
+                        drive.rightDriveMotor.setPower(Globals.MAX_MOTOR_POWER);
                     }
                     else if(offset < Globals.BEACON_MINIMUM_OFFSET) {
-                        drive.rotateDegrees(Direction.RIGHT, Globals.BEACON_ALIGN_TURN, Globals.HALF_MOTOR_POWER);
+                        drive.leftDriveMotor.setPower(Globals.MAX_MOTOR_POWER);
                     }
                     else {
                         Log.e(TAG, ERROR_MESSAGE);
