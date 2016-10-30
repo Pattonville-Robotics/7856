62a64141b4524dfbcdd1f8071804924df8ada81a
Mitchell Skaggs <skaggsm333@gmail.com>
2016-10-30 12:02:52 -0500

    Moved vision libraries and OpenCV to Common-Code

--- src/main/java/org/pattonvillerobotics/robotclasses/colordetection/BeaconColorDetection.java
+++ src/main/java/org/pattonvillerobotics/robotclasses/colordetection/BeaconColorDetection.java
@@ -5,15 +5,15 @@ import android.util.Log;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;
 import org.pattonvillerobotics.commoncode.enums.AllianceColor;
 import org.pattonvillerobotics.commoncode.enums.ColorSensorColor;
-import org.vision.ftc.resq.Beacon;
+import org.pattonvillerobotics.commoncode.opencv.android.BaseLoaderCallback;
+import org.pattonvillerobotics.commoncode.opencv.android.LoaderCallbackInterface;
+import org.pattonvillerobotics.commoncode.opencv.android.OpenCVLoader;
+import org.pattonvillerobotics.commoncode.opencv.android.Utils;
+import org.pattonvillerobotics.commoncode.opencv.core.Mat;
+import org.pattonvillerobotics.commoncode.opencv.imgproc.Imgproc;
+import org.pattonvillerobotics.commoncode.vision.ftc.resq.Beacon;
 
 import static com.qualcomm.ftccommon.DbgLog.error;
 
