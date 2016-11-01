7ecd3ebd32aa3f6dabd75974a3647c8485c582db
greg-bahr <greg.bahr123@gmail.com>
2016-11-01 16:50:15 -0500

    changed robot params

---
+++ src/main/java/org/pattonvillerobotics/opmodes/testautonomous/BeaconAlignTest.java
@@ -0,0 +1,36 @@
+package org.pattonvillerobotics.opmodes.testautonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.trailblazer.vuforia.VuforiaNav;
+import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
+import org.pattonvillerobotics.opmodes.autonomous.AutoMethods;
+import org.pattonvillerobotics.opmodes.autonomous.StartPosition;
+
+/**
+ * Created by bahrg on 11/1/16.
+ */
+@Autonomous(name="Alignment TEST")
+public class BeaconAlignTest extends LinearOpMode {
+    private VuforiaNav vuforia;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+        waitForStart();
+
+        AutoMethods.alignToBeacon();
+    }
+
+    private void initialize() {
+        AutoMethods.init(
+                new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS),
+                AllianceColor.BLUE,
+                StartPosition.LINE,
+                hardwareMap,
+                this);
+    }
+}
