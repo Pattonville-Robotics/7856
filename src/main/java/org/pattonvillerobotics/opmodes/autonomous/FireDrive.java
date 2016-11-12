524be0eb66f0ec1c3af64a2979016eca72c94fa2
greg-bahr <greg.bahr123@gmail.com>
2016-11-12 11:17:31 -0600

    added a backup autonomous

---
+++ src/main/java/org/pattonvillerobotics/opmodes/autonomous/FireDrive.java
@@ -0,0 +1,31 @@
+package org.pattonvillerobotics.opmodes.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.pattonvillerobotics.commoncode.enums.AllianceColor;
+import org.pattonvillerobotics.commoncode.enums.Direction;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
+
+/**
+ * Created by bahrg on 11/12/16.
+ */
+
+@Autonomous(name="FireDriveBackup", group="Autonomous")
+public class FireDrive extends LinearOpMode {
+
+    private EncoderDrive drive;
+    private AutoMethods autoMethods;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+        autoMethods = new AutoMethods(drive, AllianceColor.BLUE, StartPosition.LINE, hardwareMap, this);
+
+        autoMethods.fireLauncher();
+        drive.moveInches(Direction.BACKWARD, 60, Globals.MAX_MOTOR_POWER);
+
+    }
+}
