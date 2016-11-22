5d97795481ba47158f2820ff67b00902f809f832
greg-bahr <greg.bahr123@gmail.com>
2016-11-22 16:41:38 -0600

    align to beacon updates

--- src/main/java/org/pattonvillerobotics/opmodes/testautonomous/BeaconAlignTest.java
+++ src/main/java/org/pattonvillerobotics/opmodes/testautonomous/BeaconAlignTest.java
@@ -3,36 +3,45 @@ package org.pattonvillerobotics.opmodes.testautonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.pattonvillerobotics.commoncode.enums.AllianceColor;
-import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.apache.commons.math3.util.FastMath;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.trailblazer.vuforia.VuforiaNav;
 import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
-import org.pattonvillerobotics.opmodes.autonomous.AutoMethods;
-import org.pattonvillerobotics.opmodes.autonomous.StartPosition;
+import org.pattonvillerobotics.opmodes.autonomous.Globals;
 
 /**
  * Created by bahrg on 11/1/16.
  */
-@Autonomous(name="Alignment TEST")
+@Autonomous(name="Alignment TEST NUMS")
 public class BeaconAlignTest extends LinearOpMode {
     private VuforiaNav vuforia;
-    private AutoMethods autoMethods;
 
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
         waitForStart();
 
-        autoMethods.alignToBeacon();
-        while(opModeIsActive()){}
+        double x = vuforia.getxPos();
+        double y = vuforia.getDistance();
+        double Q = Globals.MINIMUM_DISTANCE_TO_BEACON;
+        double d = Math.sqrt(Math.pow(x, 2) + Math.pow((y - Q), 2));
+        double angleToTurn = FastMath.toDegrees(FastMath.atan(y/x) - FastMath.atan((y-Q)/x) );
+        double adjustmentAngle = FastMath.toDegrees(FastMath.asin(x/d));
+
+        telemetry.addData("Angle to Turn", angleToTurn);
+        telemetry.addData("Adjustment Angle", adjustmentAngle);
+        telemetry.addData("d", d);
+        telemetry.addData("x", x);
+        telemetry.addData("y", y);
+
+        //autoMethods.alignToBeacon();
+        telemetry.update();
+        while(opModeIsActive()){
+            idle();
+        }
     }
 
     private void initialize() {
-        autoMethods = new AutoMethods(
-                new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS),
-                AllianceColor.BLUE,
-                StartPosition.LINE,
-                hardwareMap,
-                this);
+        vuforia = new VuforiaNav(CustomizedRobotParameters.VUFORIA_PARAMETERS);
+        vuforia.activate();
     }
 }
