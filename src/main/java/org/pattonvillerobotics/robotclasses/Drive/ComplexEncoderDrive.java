d79d9219b85bad8d8726f7c56bb9bcc7c0184336
Michael Pieper <michaelepieper@gmail.com>
2017-01-03 14:23:39 -0600

    Changes to ComplexEncoderDrive

---
+++ src/main/java/org/pattonvillerobotics/robotclasses/Drive/ComplexEncoderDrive.java
@@ -0,0 +1,148 @@
+package org.pattonvillerobotics.robotclasses.Drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.apache.commons.math3.util.FastMath;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.pattonvillerobotics.commoncode.enums.Direction;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.RobotParameters;
+
+/**
+ * Created by pieperm on 12/30/16.
+ */
+
+public class ComplexEncoderDrive extends EncoderDrive {
+
+    public ComplexEncoderDrive(HardwareMap hardwareMap, LinearOpMode linearOpMode, RobotParameters robotParameters) {
+        super(hardwareMap, linearOpMode, robotParameters);
+    }
+
+    @Override
+    public void moveInches(Direction direction, double inches, double power) {
+
+        int targetPositionLeft;
+        int targetPositionRight;
+
+        int startPositionLeft = leftDriveMotor.getCurrentPosition();
+        int startPositionRight = rightDriveMotor.getCurrentPosition();
+
+        int deltaPosition = (int) FastMath.round(inchesToTicks(inches));
+
+        switch (direction) {
+            case FORWARD: {
+                targetPositionLeft = startPositionLeft + deltaPosition;
+                targetPositionRight = startPositionRight + deltaPosition;
+                break;
+            }
+            case BACKWARD: {
+                targetPositionLeft = startPositionLeft - deltaPosition;
+                targetPositionRight = startPositionRight - deltaPosition;
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Direction must be Direction.FORWARDS or Direction.BACKWARDS!");
+        }
+
+        DcMotor.RunMode leftDriveMotorMode = leftDriveMotor.getMode();
+        DcMotor.RunMode rightDriveMotorMode = rightDriveMotor.getMode();
+
+        leftDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftDriveMotor.setTargetPosition(targetPositionLeft);
+        rightDriveMotor.setTargetPosition(targetPositionRight);
+
+        telemetry("Moving " + inches + " inches at power " + power);
+        telemetry("LMotorT: " + targetPositionLeft);
+        telemetry("RMotorT: " + targetPositionRight);
+        telemetry("EncoderDelta: " + deltaPosition);
+        Telemetry.Item distance = telemetry("DistanceL: -1 DistanceR: -1");
+
+        // To keep power in [0.0, 1.0]. Encoders control direction
+        while (!reachedTarget(leftDriveMotor.getCurrentPosition(), targetPositionLeft, rightDriveMotor.getCurrentPosition(), targetPositionRight)) {
+            Thread.yield();
+            if (linearOpMode.isStopRequested())
+                break;
+            double t = leftDriveMotor.getCurrentPosition();
+            if(t >= t - inchesToTicks(1)) { // greater than or equal to m
+                power = ((-power * t) / (inchesToTicks(1))) + ((t * inchesToTicks(1)) / (power));
+            }
+            move(Direction.FORWARD, power);
+            distance.setValue("DistanceL: " + leftDriveMotor.getCurrentPosition() + " DistanceR: " + rightDriveMotor.getCurrentPosition());
+            linearOpMode.telemetry.update();
+        }
+        stop();
+
+        leftDriveMotor.setMode(leftDriveMotorMode); // Restore the prior mode
+        rightDriveMotor.setMode(rightDriveMotorMode);
+
+    }
+
+    @Override
+    public void rotateDegrees(Direction direction, double degrees, double power) {
+
+        int targetPositionLeft;
+        int targetPositionRight;
+
+        int startPositionLeft = leftDriveMotor.getCurrentPosition();
+        int startPositionRight = rightDriveMotor.getCurrentPosition();
+
+        double inches = degreesToInches(degrees);
+        int deltaPosition = (int) FastMath.round(inchesToTicks(inches));
+
+        switch (direction) {
+            case LEFT: {
+                targetPositionLeft = startPositionLeft - deltaPosition;
+                targetPositionRight = startPositionRight + deltaPosition;
+                break;
+            }
+            case RIGHT: {
+                targetPositionLeft = startPositionLeft + deltaPosition;
+                targetPositionRight = startPositionRight - deltaPosition;
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Direction must be Direction.LEFT or Direction.RIGHT!");
+        }
+
+        DcMotor.RunMode leftDriveMotorMode = leftDriveMotor.getMode();
+        DcMotor.RunMode rightDriveMotorMode = rightDriveMotor.getMode();
+
+        leftDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightDriveMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        leftDriveMotor.setTargetPosition(targetPositionLeft);
+        rightDriveMotor.setTargetPosition(targetPositionRight);
+
+        Telemetry.Item[] items = new Telemetry.Item[]{
+                telemetry("Rotating " + degrees + " degrees at speed " + power).setRetained(true),
+                telemetry("LMotorT: " + targetPositionLeft).setRetained(true),
+                telemetry("RMotorT: " + targetPositionRight).setRetained(true),
+                telemetry("EncoderDelta: " + deltaPosition).setRetained(true),
+                telemetry("DistanceL: DistanceR:")
+        };
+        Telemetry.Item distance = items[4];
+
+        move(Direction.FORWARD, power); // To keep speed in [0.0, 1.0]. Encoders control direction
+        while (!reachedTarget(leftDriveMotor.getCurrentPosition(), targetPositionLeft, rightDriveMotor.getCurrentPosition(), targetPositionRight)) {
+            Thread.yield();
+            if (linearOpMode.isStopRequested())
+                break;
+            double t = leftDriveMotor.getCurrentPosition();
+            if(t >= t - inchesToTicks(1)) { // greater than or equal to m
+                power = ((-power * t) / (inchesToTicks(1))) + ((t * inchesToTicks(1)) / (power));
+            }
+            distance.setValue("DistanceL: " + leftDriveMotor.getCurrentPosition() + " DistanceR: " + rightDriveMotor.getCurrentPosition());
+            linearOpMode.telemetry.update();
+        }
+        stop();
+
+        leftDriveMotor.setMode(leftDriveMotorMode); // Restore the prior mode
+        rightDriveMotor.setMode(rightDriveMotorMode);
+
+    }
+
+}
