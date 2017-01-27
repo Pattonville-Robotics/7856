090dfb704fbb40ec308a860af7da05d8f2c0f3f3
skaggsw <williampegs@gmail.com>
2017-01-27 16:43:59 -0600

    Fixed 360 degree rotation of the motor

--- src/main/java/org/pattonvillerobotics/opmodes/teleop/SpeedsterTeleOp.java
+++ src/main/java/org/pattonvillerobotics/opmodes/teleop/SpeedsterTeleOp.java
@@ -1,23 +1,24 @@
 package org.pattonvillerobotics.opmodes.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
-import org.pattonvillerobotics.opmodes.CustomizedRobotParameters;
-import org.pattonvillerobotics.opmodes.autonomous.Globals;
 
 /**
  * Created by skaggsw on 1/27/17.
  */
 
+@TeleOp(name = "Speedster")
 public class SpeedsterTeleOp extends LinearOpMode {
 
     private EncoderDrive drive;
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
 
     public void runOpMode() throws InterruptedException {
         initialize();
-        telemetry.setMsTransmissionInterval(16);
         waitForStart();
 
         while (opModeIsActive()) {
@@ -27,15 +28,21 @@ public class SpeedsterTeleOp extends LinearOpMode {
     }
 
     public void initialize() {
-        drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+        //drive = new EncoderDrive(hardwareMap, this, CustomizedRobotParameters.ROBOT_PARAMETERS);
+        motorLeft = hardwareMap.dcMotor.get("motor_left");
+        motorRight = hardwareMap.dcMotor.get("motor_right");
 
-        telemetry.setMsTransmissionInterval(16);
+        //telemetry.setMsTransmissionInterval(16);
     }
 
     public void doLoop() {
-        drive.moveFreely(Range.clip(gamepad1.left_stick_y, -Globals.MAX_MOTOR_POWER, Globals.MAX_MOTOR_POWER), Range.clip(gamepad1.right_stick_y, -Globals.MAX_MOTOR_POWER, Globals.MAX_MOTOR_POWER));
-        telemetry.addData("Left Motor Power", drive.leftDriveMotor.getPower());
-        telemetry.addData("Right Motor Power", drive.rightDriveMotor.getPower());
-        telemetry.update();
+        //drive.moveFreely(Range.clip(gamepad1.left_stick_y, -Globals.MAX_MOTOR_POWER, Globals.MAX_MOTOR_POWER), Range.clip(gamepad1.right_stick_y, -Globals.MAX_MOTOR_POWER, Globals.MAX_MOTOR_POWER));
+
+        motorLeft.setPower(gamepad1.left_stick_y);
+        motorRight.setPower(-gamepad1.right_stick_y);
+
+        //telemetry.addData("Left Motor Power", drive.leftDriveMotor.getPower());
+        //telemetry.addData("Right Motor Power", drive.rightDriveMotor.getPower());
+        //telemetry.update();
     }
 }
