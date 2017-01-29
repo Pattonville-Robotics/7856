15d46a0b036a9655a4d3c22e96a2aa2ec26f828c
skaggsw <williampegs@gmail.com>
2017-01-28 21:24:04 -0600

    Fixed 360 degree rotation of the motor

--- src/main/java/org/pattonvillerobotics/opmodes/teleop/SpeedsterTeleOp.java
+++ src/main/java/org/pattonvillerobotics/opmodes/teleop/SpeedsterTeleOp.java
@@ -3,6 +3,7 @@ package org.pattonvillerobotics.opmodes.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.pattonvillerobotics.commoncode.robotclasses.drive.EncoderDrive;
 
@@ -32,13 +33,19 @@ public class SpeedsterTeleOp extends LinearOpMode {
         motorLeft = hardwareMap.dcMotor.get("motor_left");
         motorRight = hardwareMap.dcMotor.get("motor_right");
 
+        motorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        motorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         //telemetry.setMsTransmissionInterval(16);
     }
 
     public void doLoop() {
         //drive.moveFreely(Range.clip(gamepad1.left_stick_y, -Globals.MAX_MOTOR_POWER, Globals.MAX_MOTOR_POWER), Range.clip(gamepad1.right_stick_y, -Globals.MAX_MOTOR_POWER, Globals.MAX_MOTOR_POWER));
 
-        motorLeft.setPower(gamepad1.left_stick_y);
+        motorLeft.setPower(-gamepad1.left_stick_y);
         motorRight.setPower(-gamepad1.right_stick_y);
 
         //telemetry.addData("Left Motor Power", drive.leftDriveMotor.getPower());
