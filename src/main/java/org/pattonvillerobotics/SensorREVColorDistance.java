0f89ee47d4dea8b48a2b38842ad72a6b0fbadf68
trieud01 <trieud01@student.psdr3.org>
2017-09-30 09:29:10 -0500

    Deleted large comment

--- src/main/java/org/pattonvillerobotics/SensorREVColorDistance.java
+++ src/main/java/org/pattonvillerobotics/SensorREVColorDistance.java
@@ -55,23 +55,6 @@ import java.util.Locale;
 @Autonomous(name = "Sensor: REVColorDistance", group = "Sensor") // Comment this out to add to the opmode list
 public class SensorREVColorDistance extends LinearOpMode {
 
-    /**
-     * Note that the REV Robotics Color-Distance incorporates two sensors into one device.
-     * It has a light/distance (range) sensor.  It also has an RGB color sensor.
-     * The light/distance sensor saturates at around 2" (5cm).  This means that targets that are 2"
-     * or closer will display the same value for distance/light detected.
-     *
-     * Although you configure a single REV Robotics Color-Distance sensor in your configuration file,
-     * you can treat the sensor as two separate sensors that share the same name in your op mode.
-     *
-     * In this example, we represent the detected color by a hue, saturation, and value color
-     * model (see https://en.wikipedia.org/wiki/HSL_and_HSV).  We change the background
-     * color of the screen to match the detected color.
-     *
-     * In this example, we  also use the distance sensor to display the distance
-     * to the target object.  Note that the distance sensor saturates at around 2" (5 cm).
-     *
-     */
     ColorSensor sensorColor;
     DistanceSensor sensorDistance;
 
