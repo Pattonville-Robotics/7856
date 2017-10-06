898733ff9d2f2cf5bc183a90b3d82d0dfba79afa
murphyk01 <murph754@hotmail.com>
2017-10-05 19:12:27 -0500

    Comment Testing

--- src/main/java/org/pattonvillerobotics/HolonomicTestDrive.java
+++ src/main/java/org/pattonvillerobotics/HolonomicTestDrive.java
@@ -19,7 +19,7 @@
 //
 //        drive = new HolonomicDrive(hardwareMap, this);
 //        telemetry.addData("Holonomic", "Initialized");
-// ddd
+// ddd dd
 //        waitForStart();
 //        telemetry.addData("Holonomic", "Opmode started");
 //
