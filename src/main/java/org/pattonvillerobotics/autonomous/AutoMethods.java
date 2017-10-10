a592c95a21798cff0989552edd22a937563f0baf
pieperm <michaelepieper@gmail.com>
2017-10-10 15:44:43 -0500

    CustomRobotParameters for MecanumEncoderDrive and AutoMethods class

--- src/main/java/org/pattonvillerobotics/autonomous/AutoMethods.java
+++ src/main/java/org/pattonvillerobotics/autonomous/AutoMethods.java
@@ -1,7 +1,13 @@
 package org.pattonvillerobotics.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.pattonvillerobotics.CustomRobotParameters;
 import org.pattonvillerobotics.commoncode.enums.AllianceColor;
 import org.pattonvillerobotics.commoncode.enums.Direction;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.MecanumEncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.RobotParameters;
 
 /**
  * Created by pieperm on 10/5/17.
@@ -9,23 +15,28 @@ import org.pattonvillerobotics.commoncode.enums.Direction;
 
 public class AutoMethods {
 
+    private HardwareMap hardwareMap;
+    private LinearOpMode linearOpMode;
     private AllianceColor allianceColor;
-    private Direction defaultTurnDireciton, oppositeTurnDirection;
+    private MecanumEncoderDrive drive;
 
-    public AutoMethods(AllianceColor allianceColor) {
+    public AutoMethods(HardwareMap hardwareMap, LinearOpMode linearOpMode, AllianceColor allianceColor) {
 
+        this.hardwareMap = hardwareMap;
+        this.linearOpMode = linearOpMode;
         this.allianceColor = allianceColor;
 
-        switch(allianceColor) {
-            case RED:
-                defaultTurnDireciton = Direction.LEFT;
-                oppositeTurnDirection = Direction.RIGHT;
-                break;
-            case BLUE:
-                defaultTurnDireciton = Direction.RIGHT;
-                oppositeTurnDirection = Direction.LEFT;
-                break;
-        }
+    }
+
+    public void initialize() {
+
+        drive = new MecanumEncoderDrive(hardwareMap, linearOpMode, CustomRobotParameters.ROBOT_PARAMETERS);
+
+    }
+
+    public void runAutonomousProcess() {
+
+        drive.rotateDegrees(allianceColor == AllianceColor.BLUE ? Direction.RIGHT : Direction.LEFT, 10, 0.6);
 
     }
 
