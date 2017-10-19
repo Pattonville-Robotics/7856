e948932f9624920130e0359871f0e27b44898fb4
pieperm <michaelepieper@gmail.com>
2017-10-19 09:15:36 -0500

    Globals class for constant values

---
+++ TeamCode/src/main/java/org/pattonvillerobotics/Globals.java
@@ -0,0 +1,12 @@
+package org.pattonvillerobotics;
+
+/**
+ * Created by pieperm on 10/18/17.
+ */
+
+public class Globals {
+
+    public static final double COLUMN_WIDTH = 7;
+    public static final double ROW_HEIGHT = 6.5;
+
+}
