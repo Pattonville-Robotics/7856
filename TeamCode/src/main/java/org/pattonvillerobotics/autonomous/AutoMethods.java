d5b2e728fa7facbf4e44ea4e6807c1abf91e188a
martint08 <tajhmartin87@gmail.com>
2017-10-19 19:41:28 -0500

    created driveOffBalancingStone method

--- TeamCode/src/main/java/org/pattonvillerobotics/autonomous/AutoMethods.java
+++ TeamCode/src/main/java/org/pattonvillerobotics/autonomous/AutoMethods.java
@@ -4,9 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.pattonvillerobotics.CustomRobotParameters;
+import org.pattonvillerobotics.REVGyro;
 import org.pattonvillerobotics.commoncode.enums.AllianceColor;
 import org.pattonvillerobotics.commoncode.enums.Direction;
 import org.pattonvillerobotics.commoncode.robotclasses.drive.MecanumEncoderDrive;
+import org.pattonvillerobotics.commoncode.robotclasses.drive.SimpleMecanumDrive;
 import org.pattonvillerobotics.commoncode.robotclasses.vuforia.VuforiaNavigation;
 import org.pattonvillerobotics.mechanisms.Glyphter;
 
@@ -23,6 +25,8 @@ public class AutoMethods {
     private MecanumEncoderDrive drive;
     private Glyphter glyphter;
     private VuforiaNavigation vuforia;
+    private REVGyro revGyro;
+    private SimpleMecanumDrive simpleMecanumDrive;
 
     public AutoMethods(HardwareMap hardwareMap, LinearOpMode linearOpMode, AllianceColor allianceColor) {
 
@@ -39,6 +43,7 @@ public class AutoMethods {
         drive = new MecanumEncoderDrive(hardwareMap, linearOpMode, CustomRobotParameters.ROBOT_PARAMETERS);
         glyphter = new Glyphter(hardwareMap, drive);
         vuforia = new VuforiaNavigation(CustomRobotParameters.VUFORIA_PARAMETERS);
+        simpleMecanumDrive = new SimpleMecanumDrive(linearOpMode, hardwareMap);
 
     }
 
@@ -72,6 +77,15 @@ public class AutoMethods {
 
         linearOpMode.telemetry.addData(TAG, allianceColor +  " autonomous initialized!");
 
+    }
+    private void driveOffBalancingStone (){
+        while (revGyro.getRoll() != 0 && revGyro.getPitch() != 0) {
+            if (allianceColor == AllianceColor.BLUE) {
+                simpleMecanumDrive.moveFreely(0,0.5,0);
+            }else {
+                simpleMecanumDrive.moveFreely(180,0.5,0);
+            }
+        }
     }
 
 }
